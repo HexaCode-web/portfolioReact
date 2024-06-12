e751d8aae4e0b0bd1ff3dd0132719c458ccbc6f8
mark <marcomark5050@gmail.com>
2024-06-12 05:07:00 +0300

    s

--- app/Dashboard/page.js
+++ app/Dashboard/page.js
@@ -1,15 +1,15 @@
-"use client";
+// "use client";
 
-import Navbar from "../components/nav/nav";
-import Login from "./components/login/login";
-import "./dashboard.css";
-export default function Home() {
-  return (
-    <main>
-      <Navbar />
-      <div className="Dashboard-wrapper">
-        <Login />
-      </div>
-    </main>
-  );
-}
+// import Navbar from "../components/nav/nav";
+// import Login from "./components/login/login";
+// import "./dashboard.css";
+// export default function Home() {
+//   return (
+//     <main>
+//       <Navbar />
+//       <div className="Dashboard-wrapper">
+//         <Login />
+//       </div>
+//     </main>
+//   );
+// }
